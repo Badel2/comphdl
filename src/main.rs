c256e0310d4d27d83989a268f6c8ce0dc7e010ab
Badel2 <2badel2@gmail.com>
2018-05-06 17:50:12 +0200

    Create Simulation module
    Just the run_simulation function

--- src/main.rs
+++ src/main.rs
@@ -8,61 +8,14 @@ mod emit_json;
 mod parser;
 mod bit;
 mod component;
+mod simulation;
 pub mod comphdl1;
 
-use vcd::{ Value, TimescaleUnit, SimulationCommand };
-use bit::{Bit, RepInputIterator};
+use bit::RepInputIterator;
 use component::Component;
-use std::io;
+use simulation::run_simulation;
 use std::io::Write;
 use std::fs::File;
-
-pub fn run_simulation(w: &mut io::Write,
-                  c: &mut Component,
-                  inputs: &mut Iterator<Item=Vec<Bit>>,
-                  ticks: usize) -> io::Result<()> {
-    let mut writer = vcd::Writer::new(w);
-
-    let a = c.clone_as_structural();
-    println!("{:#?}", a);
-
-    // Write the header
-    writer.timescale(1, TimescaleUnit::NS)?; // 1 tick = 1 ns
-
-    let vh = c.write_internal_components(&mut writer, &mut 0)?;
-    writer.add_module(&format!("clk"))?;
-    let clk = writer.add_wire(1, "clk")?;
-    writer.upscope()?;
-
-    writer.enddefinitions()?;
-
-    // Write the initial values
-    writer.begin(SimulationCommand::Dumpvars)?;
-    writer.change_scalar(clk, Bit::L)?;
-    // Initialize everything to X
-    for h in vh.id.values() {
-        writer.change_scalar(*h, Bit::X)?;
-    }
-    writer.end()?;
-
-    let num_inputs = c.num_inputs();
-    // Write the data values
-    let mut clk_on = true;
-    let mut t = 0;
-    for current_input in inputs.take(ticks) {
-        writer.timestamp(t)?;
-        let input_slice = current_input.len() - num_inputs;
-        let _outputs = c.update(&current_input[input_slice..input_slice + num_inputs]);
-        //println!("{:?}", outputs);
-        c.write_internal_signals(&mut writer, &mut 0, &vh)?;
-        writer.change_scalar(clk, if clk_on { Value::V1 } else { Value::V0 })?;
-        clk_on = !clk_on;
-        t += 1;
-    }
-    writer.timestamp(t)?;
-
-    Ok(())
-}
 
 fn yosys_netlist(c: &Component) {
     // We can only generate netlists from structural, not from component
