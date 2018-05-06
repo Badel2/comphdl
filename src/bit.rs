cfefab268aed8428331f078d8579d05b0749112e
Badel2 <2badel2@gmail.com>
2018-05-06 17:31:16 +0200

    Create Bit module
    With the definition of Bit and RepInputIterator

---
+++ src/bit.rs
@@ -0,0 +1,91 @@
+use vcd::Value;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum Bit {
+    L, // Low, false, 0
+    H, // High, true, 1
+    X, // Undefined
+}
+
+impl From<Bit> for Value {
+    fn from(x: Bit) -> Self {
+        match x {
+            Bit::L => Value::V0,
+            Bit::H => Value::V1,
+            Bit::X => Value::X,
+        }
+    }
+}
+
+// Returns all the n-bit combinations in order, loops infinitely
+pub struct InfiniteInputIterator {
+    current: Vec<Bit>,
+}
+
+impl InfiniteInputIterator {
+    pub fn new(n: usize) -> Self {
+        Self { current: vec![Bit::L; n] }
+    }
+}
+
+impl Iterator for InfiniteInputIterator {
+    type Item = Vec<Bit>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let a = self.current.clone();
+        self.current = next_bit_combination(&a);
+
+        Some(a)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct RepInputIterator {
+    current: Vec<Bit>,
+    count: u32,
+    max_count: u32,
+}
+
+impl RepInputIterator {
+    pub fn new(n: usize, rep: u32) -> Self {
+        Self {
+            current: vec![Bit::L; n],
+            count: rep,
+            max_count: rep,
+        }
+    }
+}
+
+impl Iterator for RepInputIterator {
+    type Item = Vec<Bit>;
+
+    fn next(&mut self) -> Option<Vec<Bit>> {
+        let a = self.current.clone();
+        self.count -= 1;
+        if self.count == 0 {
+            self.current = next_bit_combination(&a);
+            self.count = self.max_count;
+        }
+
+        Some(a)
+    }
+}
+
+fn next_bit_combination(x: &[Bit]) -> Vec<Bit> {
+    let mut y = x.to_vec();
+    let mut i = x.len();
+    let mut carry = true;
+    while carry == true && i > 0 {
+        i -= 1;
+        match x[i] {
+            Bit::L => { carry = false; y[i] = Bit::H; },
+            Bit::H => { carry = true; y[i] = Bit::L; },
+            // Incrementing X would make all the higher bits X, we don't want
+            // that, instead we keep it as X and increment the next bit
+            Bit::X => { carry = true; },
+        }
+    }
+
+    y
+}
+
--- src/main.rs
+++ src/main.rs
@@ -6,104 +6,17 @@ extern crate vcd;
 
 mod emit_json;
 mod parser;
+mod bit;
 pub mod comphdl1;
 
+use vcd::{ Value, TimescaleUnit, SimulationCommand };
+use bit::{Bit, RepInputIterator};
 use std::io;
 use std::io::Write;
 use std::fs::File;
-use vcd::{ Value, TimescaleUnit, SimulationCommand };
 use std::collections::HashMap;
 
 static VCD_SHOW_NAND: bool = true;
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub enum Bit {
-    L, // Low, false, 0
-    H, // High, true, 1
-    X, // Undefined
-}
-
-impl From<Bit> for vcd::Value {
-    fn from(x: Bit) -> Self {
-        match x {
-            Bit::L => Value::V0,
-            Bit::H => Value::V1,
-            Bit::X => Value::X,
-        }
-    }
-}
-
-// Returns all the n-bit combinations in order, loops infinitely
-pub struct InfiniteInputIterator {
-    current: Vec<Bit>,
-}
-
-impl InfiniteInputIterator {
-    pub fn new(n: usize) -> Self {
-        Self { current: vec![Bit::L; n] }
-    }
-}
-
-impl Iterator for InfiniteInputIterator {
-    type Item = Vec<Bit>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let a = self.current.clone();
-        self.current = next_bit_combination(&a);
-
-        Some(a)
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct RepInputIterator {
-    current: Vec<Bit>,
-    count: u32,
-    max_count: u32,
-}
-
-impl RepInputIterator {
-    pub fn new(n: usize, rep: u32) -> Self {
-        Self {
-            current: vec![Bit::L; n],
-            count: rep,
-            max_count: rep,
-        }
-    }
-}
-
-impl Iterator for RepInputIterator {
-    type Item = Vec<Bit>;
-
-    fn next(&mut self) -> Option<Vec<Bit>> {
-        let a = self.current.clone();
-        self.count -= 1;
-        if self.count == 0 {
-            self.current = next_bit_combination(&a);
-            self.count = self.max_count;
-        }
-
-        Some(a)
-    }
-}
-
-fn next_bit_combination(x: &[Bit]) -> Vec<Bit> {
-    let mut y = x.to_vec();
-    let mut i = x.len();
-    let mut carry = true;
-    while carry == true && i > 0 {
-        i -= 1;
-        match x[i] {
-            Bit::L => { carry = false; y[i] = Bit::H; },
-            Bit::H => { carry = true; y[i] = Bit::L; },
-            // Incrementing X would make all the higher bits X, we don't want
-            // that, instead we keep it as X and increment the next bit
-            Bit::X => { carry = true; },
-        }
-    }
-
-    y
-}
 
 pub trait Component: std::fmt::Debug {
     fn update(&mut self, input: &[Bit]) -> Vec<Bit>;
