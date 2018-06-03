258042f4b0093c2a118a735cc1f68bd33a91769f
Badel2 <2badel2@gmail.com>
2018-06-04 01:15:30 +0200

    js-beautify

--- static/index_js.js
+++ static/index_js.js
@@ -1,7 +1,7 @@
-Rust.comphdl.then( function( comphdl ) {
+Rust.comphdl.then(function(comphdl) {
     document.getElementById("loading_wasm").style.display = "none";
     document.getElementById("loaded_wasm").style.display = "block";
-    
+
     loadExample(false, "example1.txt");
     var examples = ["example1.txt", "bufbufbuf.txt", "rslatch.txt"];
     var examplesSelect = document.getElementById("exampleName");
@@ -14,15 +14,17 @@ Rust.comphdl.then( function( comphdl ) {
     });
     dragElement(document.getElementById("draggableControls"));
 });
+
 function loadExampleSelect() {
     var name = document.getElementById("exampleName").value;
     stopSimulation();
     loadExample(true, name);
 }
+
 function loadExample(force, name) {
     var a = document.getElementById("comphdl_definition");
     // Do not overwrite existing code
-    if(force == false && a.value != "") {
+    if (force == false && a.value != "") {
         return;
     }
     fetch("./comphdl_examples/" + name).then(function(response) {
@@ -36,11 +38,12 @@ function loadExample(force, name) {
         });
     });
 }
+
 function runGui() {
-    Rust.comphdl.then( function( comphdl ) {
+    Rust.comphdl.then(function(comphdl) {
         var alive = document.getElementById("check_alive");
         // TODO: check if a thread is already running and kill it
-        if(alive.checked) {
+        if (alive.checked) {
             // TODO: kill it
         }
         alive.checked = true;
@@ -50,64 +53,74 @@ function runGui() {
         resumeSimulation();
     });
 }
+
 function resumeSimulation() {
     var a = document.getElementById("check_run_forever");
     a.checked = true;
 }
+
 function pauseSimulation() {
     var a = document.getElementById("check_run_forever");
     a.checked = false;
 }
+
 function stopSimulation() {
     var a = document.getElementById("check_alive");
     a.checked = false;
 }
+
 function runStep() {
     pauseSimulation();
     var a = document.getElementById("check_run_step");
     a.checked = true;
 }
+
 function hideSimulationControls() {
     document.getElementById("simulation_controls").style.display = "none";
 }
+
 function showSimulationControls() {
     document.getElementById("simulation_controls").style.display = "block";
 }
+
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+    function elementDrag(e) {
+        e = e || window.event;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
 
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
