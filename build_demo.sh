4754c1b8a6247ebf125b189f8d585fbfda769268
Badel2 <2badel2@gmail.com>
2018-09-19 03:12:21 +0200

    Build nightly demos with travis

---
+++ build_demo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#git checkout gh-pages
+#git pull origin master
+cd netlistsvg
+npm run build-badel
+cd ..
+cargo +nightly web deploy --features stdweb
+mkdir -p demo
+rm -rf demo/nightly
+cp -rf target/deploy demo/nightly
+#git add demo
+#git commit -am 'comphdl wasm demo'
+#git push
+#git checkout master
