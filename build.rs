5a96f8677abfbecef05654b55d701a02e42a0b01
Badel2 <2badel2@gmail.com>
2018-09-20 00:06:47 +0200

    Use cargo dir convention

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ extern crate lalrpop;
 
 fn main() {
     lalrpop::Configuration::new()
+        .use_cargo_dir_conventions()
         .process_file("src/comphdl1.lalrpop")
         .unwrap();
     println!("cargo:rerun-if-changed=src/comphdl1.lalrpop");
