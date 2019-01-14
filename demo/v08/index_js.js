91bc0084fd60e4e100014ac15692079e0ddffbeb
Badel2 <2badel2@gmail.com>
2019-01-14 01:02:58 +0100

    Old demos

---
+++ demo/v08/index_js.js
@@ -0,0 +1,254 @@
+Rust.comphdl.then(function(comphdl) {
+    document.getElementById("loading_wasm").style.display = "none";
+    document.getElementById("loaded_wasm").style.display = "block";
+
+    /* Parse GET params: index.html?top=TopName&code=asdf */
+    // TODO: we should do this immediately on window load, without
+    // waiting for the Rust module
+    // Also, more options
+    var url = new URL(window.location.href);
+    var searchParams = new URLSearchParams(url.search);
+    var gotCode = searchParams.get('code');  // outputs "m2-m3-m4-m5"
+    var gotExampleName = searchParams.get('example');
+
+    if (gotCode != null) {
+        // Ideally this would create a new tab so we dont lose the existing code
+        editor.setValue(gotCode);
+    } else if (gotExampleName != null) {
+        loadExample(false, gotExampleName);
+    } else {
+        loadExample(false, "example1.txt");
+    }
+
+    var gotTop = searchParams.get('top');  // outputs "m2-m3-m4-m5"
+    if (gotTop != null) {
+        document.getElementById("top_name").value = gotTop;
+    }
+
+    var examples = ["example1.txt", "bufbufbuf.txt", "ram.txt", "srlatch.txt"];
+    if (gotExampleName == null) {
+        gotExampleName = "example1.txt";
+    }
+    var examplesSelect = document.getElementById("exampleName");
+    examples.forEach(function(e, i) {
+        var option = document.createElement('option');
+        option.selected = e == gotExampleName;
+        option.value = e;
+        option.text = e;
+        examplesSelect.append(option);
+    });
+    dragElement(document.getElementById("draggableControls"));
+});
+
+function register_main_loop(main_loop) {
+        var check_run_forever = document.getElementById("check_run_forever");
+        var check_run_step = document.getElementById("check_run_step");
+        var check_alive = document.getElementById("check_alive");
+        var tick_display = document.getElementById("tick_display");
+        var check_show_debug = document.getElementById("check_show_debug");
+        var check_show_signals = document.getElementById("check_show_signals");
+        var target_ticks_per_second = document.getElementById("target_ticks_per_second");
+        var tick = 0;
+        var intervalId;
+
+        function demo() {
+            if(check_run_forever.checked || check_run_step.checked) {
+                main_loop(check_show_debug.checked, check_show_signals.checked);
+                check_run_step.checked = false;
+                tick += 1;
+                tick_display.value = tick;
+            }
+
+            if(check_alive.checked == false) {
+                // Stop running
+                main_loop.drop(); // Necessary to clean up the closure on Rust's side.
+                clearInterval(intervalId);
+            } else {
+                /*
+                // Can we use setInterval if the function takes more than 1000/30 ms
+                // to run? Yes, js is singlethreaded.
+                var fps = parseInt(target_ticks_per_second.value, 10);
+                if(isNaN(fps)) { fps = 30; }
+                setTimeout(demo, 1000/fps);
+                */
+            }
+        }
+
+        intervalId = setInterval(demo, 1000/30);
+}
+
+function loadExampleSelect() {
+    var name = document.getElementById("exampleName").value;
+    stopSimulation();
+    loadExample(true, name);
+}
+
+function loadExample(force, name) {
+    var editorValue = editor.getValue();
+    // Do not overwrite existing code
+    if (force == false && editorValue != "") {
+        return;
+    }
+    fetch("./comphdl_examples/" + name).then(function(response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response;
+    }).then(function(response) {
+        response.text().then(function(text) {
+            editor.setValue(text);
+        });
+    });
+}
+
+function shareCode() {
+    var URLWithoutParams = [window.location.protocol, '//', window.location.host, window.location.pathname].join('');
+
+    var code = editor.getValue();
+    var codeURI = encodeURIComponent(code);
+
+    var topName = document.getElementById("top_name").value;
+    var topNameURI = encodeURIComponent(topName);
+
+    var fullURL = URLWithoutParams + '?top=' + topNameURI + '&code=' + codeURI;
+
+    share_div = document.getElementById("shareCodeDiv");
+    share_div.style.display = "block";
+    share_input = document.getElementById("shareCodeLink");
+    share_input.value = fullURL;
+}
+
+function runGui() {
+    Rust.comphdl.then(function(comphdl) {
+        var alive = document.getElementById("check_alive");
+        // TODO: check if a thread is already running and kill it
+        if (alive.checked) {
+            // TODO: kill it
+            alert("Stop the program before running it again");
+            // We cannot kill it here because the Rust code will not update
+            // and we get a classical race condition.
+            // This could maybe be fixed by giving each Rust instance its own
+            // uuid, and have a field "running_uuid" which must match or the
+            // program will stop
+            return;
+        }
+        alive.checked = true;
+        var error_string = comphdl.run_js_gui();
+        console.log(error_string);
+        document.getElementById("top_output_debug").value = error_string;
+        showSimulationControls();
+        document.getElementById("renderButton").onclick().then(function(svg) {
+            // Enable clicking to svg ports toggles inputs
+            var ti = document.getElementById("top_input");
+            for(var i=0; i<ti.children.length; i++) {
+                var tic = ti.children[i];
+                var inputid = tic.id.replace("checkbox_input_", "inputExt_");
+                tic.onclick = function(){
+                    var inputid = this.id.replace("checkbox_input_", "inputExt_");
+                    var ig = document.getElementById(inputid);
+                    if(ig) {
+                        if(this.checked) {
+                            ig.style = "fill: #70FF70;";
+                        } else {
+                            ig.style = "fill: #147014;";
+                        }
+                    }
+                };
+                var ig = document.getElementById(inputid);
+                if(ig) {
+                    ig.style = "fill: #147014;";
+                    ig.onclick = function(){
+                        var checkboxid = this.id.replace("inputExt_", "checkbox_input_");
+                        document.getElementById(checkboxid).click();
+                    };
+                } else {
+                    console.error(tic.id + " is missing the corresponding " + inputid + " in the svg")
+                }
+            }
+
+            resumeSimulation();
+        });
+    });
+}
+
+function resumeSimulation() {
+    var a = document.getElementById("check_run_forever");
+    a.checked = true;
+}
+
+function pauseSimulation() {
+    var a = document.getElementById("check_run_forever");
+    a.checked = false;
+}
+
+function stopSimulation() {
+    var a = document.getElementById("check_alive");
+    a.checked = false;
+}
+
+function runStep() {
+    pauseSimulation();
+    var a = document.getElementById("check_run_step");
+    a.checked = true;
+}
+
+function hideSimulationControls() {
+    document.getElementById("simulation_controls").style.display = "none";
+}
+
+function showSimulationControls() {
+    document.getElementById("simulation_controls").style.display = "block";
+}
+
+function toggleFloatingControls() {
+    var a = document.getElementById("draggableControls");
+    if (a.style.position == 'absolute') {
+        a.style.position = 'static';
+        a.style.width = '';
+    } else {
+        a.style.position = 'absolute';
+        a.style.width = '500px';
+    }
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
