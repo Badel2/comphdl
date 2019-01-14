262b25f0e8faec7c8b866c609acfa1a9536638fd
Badel2 <2badel2@gmail.com>
2019-01-14 01:38:19 +0100

    Only install cargo-web if not already installed

--- ci/install_cargo_web.sh
+++ ci/install_cargo_web.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Only install if not already installed
+cargo-web -V && exit 0
+
 CARGO_WEB_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/koute/cargo-web/releases/latest)
 CARGO_WEB_VERSION=$(echo $CARGO_WEB_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 CARGO_WEB_URL="https://github.com/koute/cargo-web/releases/download/$CARGO_WEB_VERSION/cargo-web-x86_64-unknown-linux-gnu.gz"
